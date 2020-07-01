97036b859e99ed769c2c6960a5e049d205f9bfa9
lukasz26671 <lukasz26671@wp.pl>
2020-07-01 18:44:06 +0200

    single website cors

--- app.ts
+++ app.ts
@@ -18,7 +18,7 @@ let httpServer = app.listen(port, ()=>{
 })
 
 app.get('/', (req, res) => {
-    res.header('Access-Control-Allow-Origin: *')
+    res.setHeader('Access-Control-Allow-Origin',  'https://lukasz26671.github.io')
     res.status(200)
 });
 
