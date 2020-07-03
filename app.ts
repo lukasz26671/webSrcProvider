3fdfd692d2008919581964d4a04b9256d1518925
lukasz26671 <lukasz26671@wp.pl>
2020-07-03 23:10:57 +0200

    formatting

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import {google} from 'googleapis'
+import { google } from 'googleapis'
 import * as express from 'express'
 
 const apikey = process.env.APIKEY
@@ -11,7 +11,7 @@ const gSheets = google.sheets({
 
 const app = express();
 
-let httpServer = app.listen(port, ()=>{
+let httpServer = app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
 
@@ -25,7 +25,7 @@ app.get('/readplaylist', (req, res) => {
 
     res.setHeader('Content-Type', "application/json")
     console.log('Sent headers: ', res.headersSent)
-    GetSources.then((data : SourceResponse) => {
+    GetSources.then((data: SourceResponse) => {
         res.send(data)
     })
 })
@@ -46,52 +46,52 @@ var GetSources = new Promise((resolve, reject) => {
             spreadsheetId: '1JhbSnAQdcs4QGnCUx6fZ0ujV9G2k-Wjvs1YoTmoD2i0',
             ranges: ['C3:C123', 'D3:D123', 'E3:E123']
         }, (err, res) => {
-            if(err) return console.log('Error ' + err)
-        
-            const response : SourceResponse = {
+            if (err) return console.log('Error ' + err)
+
+            const response: SourceResponse = {
                 "authors": [],
-                "titles": [], 
+                "titles": [],
                 "IDs": []
             }
-    
-            
 
-            let i : number;
+
+
+            let i: number;
             res.data.valueRanges[0].values.forEach(value => {
-                if(value[0] != '') {
+                if (value[0] != '') {
                     response.authors.push(value[0])
                     i++;
                 } else {
-                    if(!emptyIndexes.includes(i))
+                    if (!emptyIndexes.includes(i))
                         emptyIndexes.push(i)
                 }
             });
-            let j : number;
+            let j: number;
             res.data.valueRanges[1].values.forEach(value => {
-                if(value[0] != '') {
+                if (value[0] != '') {
                     response.titles.push(value[0])
                     j++;
                 } else {
-                    if(!emptyIndexes.includes(j))
+                    if (!emptyIndexes.includes(j))
                         emptyIndexes.push(j)
                 }
             });
-            let k : number;
+            let k: number;
             res.data.valueRanges[2].values.forEach(value => {
-                if(value[0] != '') {
+                if (value[0] != '') {
                     response.IDs.push(value[0])
                     k++;
                 } else {
-                    if(!emptyIndexes.includes(k))
+                    if (!emptyIndexes.includes(k))
                         emptyIndexes.push(k)
                 }
             });
-            
-            
+
+
             i = 0;
-            response.authors.map(elem => { 
-                if(elem != '') i++
-                if(emptyIndexes.includes(i)) {
+            response.authors.map(elem => {
+                if (elem != '') i++
+                if (emptyIndexes.includes(i)) {
                     response.IDs.splice(i, 1);
                     console.log(`Removed - ROW(AUTHORS) @ index ${i} from response`)
                 }
@@ -99,8 +99,8 @@ var GetSources = new Promise((resolve, reject) => {
             })
             j = 0;
             response.titles.map(elem => {
-                if(elem != '') j++
-                if(emptyIndexes.includes(j)) {
+                if (elem != '') j++
+                if (emptyIndexes.includes(j)) {
                     response.IDs.splice(j, 1);
                     console.log(`Removed - ROW(TITLES) @ index ${j} from response`)
                 }
@@ -108,17 +108,16 @@ var GetSources = new Promise((resolve, reject) => {
             })
             k = 0;
             response.IDs.map(elem => {
-                if(elem != '') k++
-                if(emptyIndexes.includes(k)) {
+                if (elem != '') k++
+                if (emptyIndexes.includes(k)) {
                     response.IDs.splice(k, 1);
                     console.log(`Removed - ROW(IDS) @ index ${k} from response`)
                 }
 
-                return elem.toUpperCase();
             })
 
             resolve(response)
-        }) 
+        })
     } catch (error) {
         reject(error)
     }
